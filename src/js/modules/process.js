28af7c087138a5288492215d0a0a9630adc1cb84
y-nagai0725 <y.nagai0725@gmail.com>
2026-08-13 01:30:29 +0900

    feat: 背景レイヤー配置、Processセクションにて背景色を変化させる処理実装。各セクションでの背景色設定を削除。

--- src/js/modules/process.js
+++ src/js/modules/process.js
@@ -9,10 +9,14 @@ import { BREAKPOINTS, COLORS, GSAP_CONFIG } from '../utils/constants.js';
 gsap.registerPlugin(ScrollTrigger);
 
 export const initProcess = () => {
+  const globalBg = document.querySelector('.js-global-bg');
   const processSection = document.querySelector('.js-process-bg');
+  const textWrap = document.querySelector('.js-process-text-wrap');
   const processTitle = document.querySelector('.js-process-title');
-  const texts = document.querySelectorAll('.js-process-text');
+  const processTexts = document.querySelectorAll('.js-process-lead, .js-process-desc');
   const images = document.querySelectorAll('.js-process-image');
+
+  if (!globalBg || !processSection || !textWrap || !processTitle || processTexts.length === 0 || images.length === 0) return;
 
   // ------------------------------
   // 背景とテキストのカラーチェンジ
@@ -21,19 +25,24 @@ export const initProcess = () => {
     scrollTrigger: {
       trigger: processSection,
       start: 'top 50%',
-      toggleActions: 'play none none reverse',
+      end: 'bottom bottom',
+      toggleActions: 'play reverse play reverse',
     }
   });
 
-  tl.to(processSection, {
+  tl.to(globalBg, {
     backgroundColor: COLORS.LIGHT,
-    color: COLORS.DARK,
-    duration: 1.5,
+    duration: 1.0,
     ease: GSAP_CONFIG.EASE,
   }, 0)
     .to(processTitle, {
       color: COLORS.DARK,
-      duration: 1.5,
+      duration: 1.0,
+      ease: GSAP_CONFIG.EASE,
+    }, 0)
+    .to(processTexts, {
+      color: COLORS.DARK,
+      duration: 1.0,
       ease: GSAP_CONFIG.EASE,
     }, 0);
 
@@ -64,8 +73,10 @@ export const initProcess = () => {
 
     if (isPc) {
       // PC時：横並びなので、テキストと画像を一気にStaggerで登場させる
-      gsap.fromTo([...texts, ...images],
-        commonFrom,
+      gsap.fromTo([textWrap, ...images],
+        {
+          ...commonFrom
+        },
         {
           ...commonTo,
           stagger: 0.2, // テキスト→レモン→麦→樽の順
@@ -79,8 +90,10 @@ export const initProcess = () => {
       // SP時：縦積みなので、テキストと画像を別々のトリガーで発火させる
 
       // 上にあるテキストのアニメーション
-      gsap.fromTo(texts,
-        commonFrom,
+      gsap.fromTo(textWrap,
+        {
+          ...commonFrom
+        },
         {
           ...commonTo,
           scrollTrigger: {
@@ -92,7 +105,9 @@ export const initProcess = () => {
 
       // 下にある画像のアニメーション
       gsap.fromTo(images,
-        commonFrom,
+        {
+          ...commonFrom
+        },
         {
           ...commonTo,
           stagger: 0.2, // 3枚の画像は順番に出す
