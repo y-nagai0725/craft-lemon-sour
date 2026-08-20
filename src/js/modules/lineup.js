219fd7767fcfcc49e1117c1578d027a2bee55229
y-nagai0725 <y.nagai0725@gmail.com>
2026-08-20 14:47:01 +0900

    fix: sp表示時とpc表示時で横スクロールアニメーション設定を別々にするように変更

--- src/js/modules/lineup.js
+++ src/js/modules/lineup.js
@@ -36,103 +36,119 @@ export const initLineup = () => {
     const baseAmount = getScrollAmount();
 
     // SPの時はスクロール量を増やしてゆっくり見せる
-    // 画面幅がMD（768px）未満なら2倍、以上なら1倍（そのまま）にする
-    const multiplier = window.innerWidth < BREAKPOINTS.MD ? 2 : 1;
+    // 画面幅がMD（768px）未満なら1.75倍、以上なら1倍（そのまま）にする
+    const multiplier = window.innerWidth < BREAKPOINTS.MD ? 1.75 : 1;
     return `+=${baseAmount * multiplier}`;
   };
 
-  // ---------------------------------------------------
-  // 横スクロールのアニメーション
-  // ---------------------------------------------------
-  const scrollTween = gsap.to(track, {
-    x: () => -getScrollAmount(),
-    ease: 'none',
-    scrollTrigger: {
-      trigger: trigger,
-      pin: ".js-lineup-pin",
-      scrub: 1,
-      start: 'top top',
-      end: getScrollEnd,
-      invalidateOnRefresh: true,
-    }
-  });
+  const mm = gsap.matchMedia();
 
-  // ---------------------------------------------------
-  // インジケーターのアニメーション
-  // ---------------------------------------------------
+  mm.add({
+    isSp: `(width < ${BREAKPOINTS.MD}px)`,
+    isPc: `(width >= ${BREAKPOINTS.MD}px)`,
+  }, (context) => {
+    let { isSp, isPc } = context.conditions;
 
-  // 横スクロールに連動するタイムラインを作成
-  const progressTl = gsap.timeline({
-    scrollTrigger: {
-      trigger: trigger,
-      start: 'top top',
-      end: getScrollEnd,
-      scrub: 1,
-      invalidateOnRefresh: true,
-    }
-  });
+    // scrollTriggerのscrub設定（PC: 少し遅らせる, SP: 1対1の設定）
+    const scrubValue = isPc ? 1 : true;
 
-  // 取得したバーを、順番に scaleX: 1 (100%) にしていく
-  progressBars.forEach((bar) => {
-    progressTl.to(bar, {
-      scaleX: 1,
-      ease: 'none', // スクロール量に比例させるために 'none' にする
+    // ---------------------------------------------------
+    // 横スクロールのアニメーション
+    // ---------------------------------------------------
+    const scrollTween = gsap.to(track, {
+      x: () => -getScrollAmount(),
+      ease: 'none',
+      scrollTrigger: {
+        trigger: trigger,
+        pin: ".js-lineup-pin",
+        scrub: scrubValue,
+        start: 'top top',
+        end: getScrollEnd,
+        invalidateOnRefresh: true,
+      }
     });
-  });
 
-  // ---------------------------------------------------
-  // 各アイテムを順番に表示させる
-  // ---------------------------------------------------
+    // ---------------------------------------------------
+    // インジケーターのアニメーション
+    // ---------------------------------------------------
 
-  // .fromの共通設定
-  const commonFrom = {
-    scale: 0.9,
-    filter: 'blur(8px)',
-    autoAlpha: 0,
-  };
+    // 横スクロールに連動するタイムラインを作成
+    const progressTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: trigger,
+        start: 'top top',
+        end: getScrollEnd,
+        scrub: scrubValue,
+        invalidateOnRefresh: true,
+      }
+    });
 
-  // .toの共通設定
-  const commonTo = {
-    scale: 1,
-    filter: 'blur(0px)',
-    autoAlpha: 1,
-    duration: 1.2,
-    ease: 'power2.out',
-  };
+    // 取得したバーを、順番に scaleX: 1 (100%) にしていく
+    progressBars.forEach((bar) => {
+      progressTl.to(bar, {
+        scaleX: 1,
+        ease: 'none', // スクロール量に比例させるために 'none' にする
+      });
+    });
 
-  items.forEach((item, index) => {
-    const itemImage = item.querySelector(".js-lineup-image"); // アイテム内の画像要素
-    if (index === 0) {
-      // 最初のアイテムは、「縦スクロール」で発火させる
-      gsap.fromTo(item,
-        {
-          ...commonFrom,
-        },
-        {
-          ...commonTo,
-          scrollTrigger: {
-            trigger: itemImage,
-            start: 'top 65%',
-            toggleActions: 'play none none reverse',
-          }
-        }
-      );
-    } else {
-      // 2番目以降のアイテムは、「横スクロール」に連動させて発火させる
-      gsap.fromTo(item,
-        {
-          ...commonFrom,
-        },
-        {
-          ...commonTo,
-          scrollTrigger: {
-            trigger: itemImage,
-            containerAnimation: scrollTween, // 横スクロールと連動
-            start: 'left 80%', // 右から入ってきたら発火
-            toggleActions: 'play none none reverse',
-          }
-        }
-      );
+    // ---------------------------------------------------
+    // 各アイテムを順番に表示させる
+    // ---------------------------------------------------
+
+    // .fromの共通設定
+    const commonFrom = {
+      scale: 0.9,
+      autoAlpha: 0,
+    };
+
+    // .toの共通設定
+    const commonTo = {
+      scale: 1,
+      autoAlpha: 1,
+      duration: 1.2,
+      ease: 'power2.out',
+    };
+
+    // pc表示時はアニメーション対象に filter を追加
+    if (isPc) {
+      commonFrom.filter = 'blur(8px)';
+      commonTo.filter = 'blur(0px)';
     }
+
+    items.forEach((item, index) => {
+      const itemImage = item.querySelector(".js-lineup-image"); // アイテム内の画像要素
+      if (index === 0) {
+        // 最初のアイテムは、「縦スクロール」で発火させる
+        gsap.fromTo(item,
+          {
+            ...commonFrom,
+          },
+          {
+            ...commonTo,
+            scrollTrigger: {
+              trigger: itemImage,
+              start: 'top 65%',
+              toggleActions: 'play none none reverse',
+            }
+          }
+        );
+      } else {
+        // 2番目以降のアイテムは、「横スクロール」に連動させて発火させる
+        gsap.fromTo(item,
+          {
+            ...commonFrom,
+          },
+          {
+            ...commonTo,
+            scrollTrigger: {
+              trigger: itemImage,
+              containerAnimation: scrollTween, // 横スクロールと連動
+              start: 'left 80%', // 右から入ってきたら発火
+              toggleActions: 'play none none reverse',
+            }
+          }
+        );
+      }
+    });
   });
 };
